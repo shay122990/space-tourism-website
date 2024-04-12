a2e8abeec6b30458e3440634a9cef179175915f7
Shay <moeshakasanova@gmail.com>
2024-04-12 20:44:58 +0400

    navbar tablet/mobile, home page, background image change home page

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,24 +1,22 @@
-
 import './Home.css';
+import { Link } from 'react-router-dom';
 const Home = () => {
 
   return (
     <div className="home-container">
-     <div className="home-info-container">
       <div className="space-info-container">
         <div className="main-info">
-        <h2 className='intro'>So, you want to travel to</h2>
-          <span className='space'> Space</span>
+        <p className='intro'>So, you want to travel to</p>
+          <h1 className='space'> Space</h1>
           <p className='space-info'>Let’s face it; if you want to go to space, you might as well genuinely go to 
           outer space and not hover kind of on the edge of it. Well sit back, and relax 
           because we’ll give you a truly out of this world experience!
            </p>
         </div>
         <div className="explore-btn-container">
-          <button className='explore-btn'>Explore</button>
+          <Link to='/destination'><button className='explore-btn'>Explore</button></Link>
         </div>
       </div>
-     </div>
     </div>
   )
 }
