b4f9091f2dd9308adba12ab538e166f7274194eb
Shay <moeshakasanova@gmail.com>
2024-05-16 16:58:16 +0400

    removed button from crew.jsx

--- src/routes/crew/Crew.jsx
+++ src/routes/crew/Crew.jsx
@@ -1,67 +1,58 @@
-//
 import { useState } from "react";
 import "./Crew.css";
 import crewData from "../../data/crew.js";
 
 const Crew = () => {
-  const [selectedCrewMember, setSelectedCrewMember] = useState(crewData[0]);
+  const [selectedCrew, setSelectedCrew] = useState(crewData[0]);
 
-  const handleTabChange = (crewMember) => {
-    setSelectedCrewMember(crewMember);
+  const handleTabChange = (crew) => {
+    setSelectedCrew(crew);
   };
 
   return (
     <div className="crew-home-page">
-      <main className="main-crew-container">
+      <div className="main-crew-container">
         <h1 className="meet-your-crew">
           <strong>02</strong> Meet your crew
         </h1>
         <div className="crew-content">
           <div className="crew-details">
             <div className="crew-info">
-              <h2 className="crew-role">{selectedCrewMember.role}</h2>
-              <h3 className="crew-name">{selectedCrewMember.name}</h3>
-              <p className="crew-bio">{selectedCrewMember.bio}</p>
+              <h2 className="crew-role">{selectedCrew.role}</h2>
+              <h3 className="crew-name">{selectedCrew.name}</h3>
+              <p className="crew-bio">{selectedCrew.bio}</p>
             </div>
             <nav
               className="crew-tab-menu"
               role="navigation"
               aria-label="Crew Tabs"
             >
-              <ul role="tablist">
-                {crewData.map((crewMember) => (
-                  <li key={crewMember.id} role="presentation">
-                    <button
-                      role="tab"
-                      tabIndex={0}
-                      className={
-                        selectedCrewMember.id === crewMember.id ? "active" : ""
+              <ul>
+                {crewData.map((crew) => (
+                  <li
+                    key={crew.id}
+                    role="tab"
+                    tabIndex={0}
+                    className={selectedCrew.id === crew.id ? "active" : ""}
+                    onClick={() => handleTabChange(crew)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter" || e.key === " ") {
+                        handleTabChange(crew);
                       }
-                      onClick={() => handleTabChange(crewMember)}
-                      onKeyDown={(e) => {
-                        if (e.key === "Enter" || e.key === " ") {
-                          handleTabChange(crewMember);
-                        }
-                      }}
-                      aria-selected={selectedCrewMember.id === crewMember.id}
-                      aria-controls={`crew-panel-${crewMember.id}`}
-                      id={`crew-tab-${crewMember.id}`}
-                    ></button>
-                  </li>
+                    }}
+                    aria-selected={selectedCrew.id === crew.id}
+                    aria-controls={`crew-panel-${crew.id}`}
+                    id={`crew-tab-${crew.id}`}
+                  ></li>
                 ))}
               </ul>
             </nav>
           </div>
-          <div
-            id={`crew-panel-${selectedCrewMember.id}`}
-            role="tabpanel"
-            aria-labelledby={`crew-tab-${selectedCrewMember.id}`}
-            className="crew-image"
-          >
-            <img src={selectedCrewMember.image} alt={selectedCrewMember.name} />
+          <div id={`crew-panel-${selectedCrew.id}`} className="crew-image">
+            <img src={selectedCrew.image} alt={selectedCrew.name} />
           </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 };
