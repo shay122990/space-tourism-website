67602045d5b6113a781d58dc17e70ea5b1c5f6a9
Shay <moeshakasanova@gmail.com>
2024-12-04 09:57:54 +0400

    destination styaling

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -3,22 +3,20 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <main className="home-container">
-      <div className="space-info-container">
-        <main className="main-info">
-          <p className="intro">So, you want to travel to</p>
-          <h1 className="space"> Space</h1>
-          <p className="space-info">
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
-        </main>
-        <div className="explore-btn-container">
-          <Link to="/destination">
-            <button className="explore-btn">Explore</button>
-          </Link>
-        </div>
+      <div className="main-info">
+        <p className="intro">So, you want to travel to</p>
+        <h1 className="space"> Space</h1>
+        <p className="space-info">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <div className="explore-btn-container">
+        <Link to="/destination">
+          <button className="explore-btn">Explore</button>
+        </Link>
       </div>
     </main>
   );
